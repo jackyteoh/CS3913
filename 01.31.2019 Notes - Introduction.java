bce640ee9e805e35c245a4158495d46f1e781543
Jacky Teoh <jt2908@nyu.edu>
2019-02-03 12:27:24 -0500

    01.31.2019

---
+++ 01.31.2019 Notes - Introduction.java
@@ -0,0 +1,16 @@
+// 01.31.2019 Java Lecture Notes Class 1
+
+// Everything in Java exists inside of an object (even main)
+// Syntax very similar to C++
+
+// Only one public class per file.
+// This file must be named Demo1 if you want to run it. Because the public class is called Demo1
+
+public class Demo1{ // Java requires you to name the file the same as the public class inside the file. 
+	public static void main(String args[]){
+		System.out.println("Hello World");
+	}
+}
+
+// Download NetBeans IDE, latest JDK, latest JRE NetBeans installs all for you when they're downloaded
+// Build --> run
